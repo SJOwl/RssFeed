82b00d38f61b5be2fbb6ed1bb9ca19dbc57917ef
Evgeniy <vorobeievgeniy@gmail.com>
2018-04-13 20:17:38 +0300

    init

---
+++ app/src/main/java/au/sj/owl/templateproject/App.kt
@@ -0,0 +1,14 @@
+package au.sj.owl.templateproject
+
+import android.app.Application
+import timber.log.Timber
+import timber.log.Timber.DebugTree
+
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        if (BuildConfig.DEBUG) {
+            Timber.plant(DebugTree())
+        }
+    }
+}
